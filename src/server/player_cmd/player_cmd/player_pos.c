2118b76ade7b3b8f8a651cbe0af5de3df97c1f9c
Thery Fouchter <thery.fouchter@epitech.eu>
2018-06-17 23:54:32 +0200

    implemented player level & pos

--- src/server/player_cmd/player_cmd/player_pos.c
+++ src/server/player_cmd/player_cmd/player_pos.c
@@ -9,9 +9,23 @@
 #include "player_callback.h"
 #include "debug.h"
 #include "player.h"
+#include "map_it.h"
+
+static int	direction_to_int_spec(direction_t d)
+{
+	return d & DIR_UP ? 1 : d & DIR_RIGHT ? 2 : d & DIR_DOWN ? 3 : 4;
+}
 
 void	spectate_cmd_player_pos(player_cmd_arg_t *args)
 {
-	//todo player pos
-	(void)args;DEBUG("%s", __func__);
+	player_t	*player;
+
+	player = get_player_from_id(args->server->map, atoi(args->args[1]));
+	if (!player)
+		return;
+	client_callback(CB_PLAYER_POSITION, args->client,
+		player->id,
+		player->pos->pos.x,
+		player->pos->pos.y,
+		direction_to_int_spec(player->dir));
 }
