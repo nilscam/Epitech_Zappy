770f341403af6131d7324eb1137998fcf7ebb0c6
Thery Fouchter <thery.fouchter@epitech.eu>
2018-06-17 23:14:55 +0200

    implemented player inventory

--- src/server/player_cmd/player_cmd/player_inventory.c
+++ src/server/player_cmd/player_cmd/player_inventory.c
@@ -9,9 +9,24 @@
 #include "player_callback.h"
 #include "debug.h"
 #include "player.h"
+#include "map_it.h"
 
 void	spectate_cmd_player_inventory(player_cmd_arg_t *args)
 {
-	//todo player inv
-	(void)args;DEBUG("%s", __func__);
+	player_t	*player;
+
+	player = get_player_from_id(args->server->map, atoi(args->args[1]));
+	if (!player)
+		return;
+	client_callback(CB_PLAYER_INVENTORY, args->client,
+		player->id,
+		player->pos->pos.x,
+		player->pos->pos.y,
+		player->inventory.food,
+		player->inventory.stones[LINEMATE],
+		player->inventory.stones[DERAUMERE],
+		player->inventory.stones[SIBUR],
+		player->inventory.stones[MENDIANE],
+		player->inventory.stones[PHIRAS],
+		player->inventory.stones[THYSTAME]);
 }
