f5bd44c723560d910ec1c41385bff7e304b66d17
Thery Fouchter <thery.fouchter@epitech.eu>
2018-06-18 18:07:33 +0200

    Improved big buff

--- src/server/big_buff/big_buff.h
+++ src/server/big_buff/big_buff.h
@@ -8,22 +8,29 @@
 #ifndef big_buff_HPP
 #define big_buff_HPP
 
+#include "list.h"
+#include "list_iterator.h"
+#include "str_concat.h"
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 #define BIG_BUFF_SIZE	10000
 
 typedef struct
 {
+	list_t	list;
 	char	buff[BIG_BUFF_SIZE];
 	int	pos;
 }	big_buff_t;
 
 /* big_buff.c */
-void	big_buff_reset(big_buff_t *buff);
-bool	big_buff_can_add_data(big_buff_t *buff, size_t size_data);
-void	big_buff_add_data(big_buff_t *buff, char *data, size_t size_data);
-char	*big_buff_strdup(big_buff_t *buff);
+void	big_buff_init(big_buff_t *buff);
+bool	big_buff_add_data(big_buff_t *buff, char *data, size_t size_data);
+bool	big_buff_add_format(big_buff_t *buff, char *format, ...);
+char	*big_buff_cpy_data(big_buff_t *buff);
+void	big_buff_deinit(big_buff_t *buff);
 
 #endif // !big_buff_HPP
