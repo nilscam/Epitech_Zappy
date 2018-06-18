eeeae63c7bebe380a3f385f34f908658cea902dc
Thery Fouchter <thery.fouchter@epitech.eu>
2018-06-18 18:37:42 +0200

    Added map stats

---
+++ src/server/map/stat.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2017
+** File description:
+** stat.c
+*/
+
+#include "map_it.h"
+
+static void	count_stones(inventory_t *from, inventory_t *to)
+{
+	to->food += from->food;
+	to->stones[LINEMATE] += from->stones[LINEMATE];
+	to->stones[DERAUMERE] += from->stones[DERAUMERE];
+	to->stones[SIBUR] += from->stones[SIBUR];
+	to->stones[MENDIANE] += from->stones[MENDIANE];
+	to->stones[PHIRAS] += from->stones[PHIRAS];
+	to->stones[THYSTAME] += from->stones[THYSTAME];
+}
+
+void	count_inv_in_map(map_t *self, inventory_t *inv)
+{
+	for (int y = 0; y < self->size.y; ++y) {
+		for (int x = 0; x < self->size.x; ++x) {
+			count_stones(&self->cases[y][x].inventory, inv);
+		}
+	}
+}
