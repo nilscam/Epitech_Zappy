26cb97adea606581daebf38ed48aa4617b401756
Unknown <nilscambreleng@gmail.com>
2018-06-24 11:59:51 -0700

    makefile zappy ai

--- test.sh
+++ test.sh
@@ -16,26 +16,8 @@
 ./zappy_ai -p 4242 -n blue &
 ./zappy_ai -p 4242 -n blue &
 ./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
 
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
+
 ./zappy_ai -p 4242 -n red &
 ./zappy_ai -p 4242 -n red &
 ./zappy_ai -p 4242 -n red &
