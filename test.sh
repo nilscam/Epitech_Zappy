faa9c325a1abbc04a7761b0a0f23b2f675b0e221
Thery Fouchter <thery.fouchter@epitech.eu>
2018-06-24 21:15:18 +0200

    better test.sh

--- test.sh
+++ test.sh
@@ -1,53 +1,29 @@
 
 #!/bin/bash
 
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
-./zappy_ai -p 4242 -n blue &
+if [ $# -lt 4 ]
+then
+	echo "USAGE: $0 IP PORT NB_AI [ teams ... ]"
+	exit 0
+fi
 
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
-./zappy_ai -p 4242 -n red &
+exe='./zappy_ai'
+
+if [ ! -f "$exe" ]
+then
+	echo "'$exe' doesn't exist"
+	exit 1
+fi
+
+ip=$1
+port=$2
+ai=$3
+
+for ((i=1; i<=$ai; i++))
+do
+	for team in "${@:4}"
+	do
+		$exe -p $port -n $team -h $ip &
+	done
+	
+done
